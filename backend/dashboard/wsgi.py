51a8222804d276c4b9b81751cee10e5ff68a7111
Tofunori <124537575+tofunori@users.noreply.github.com>
2025-03-19 08:53:58 -0400

    Add Django WSGI configuration

---
+++ backend/dashboard/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config for dashboard project."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+
+application = get_wsgi_application()
